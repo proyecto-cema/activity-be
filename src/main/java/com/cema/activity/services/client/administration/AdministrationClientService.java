f67ecc3fe92615be9345166ac8c15ca1bd6f1031
Merlin Nuñez <merlinsn@gmail.com>
2021-12-10 18:46:10 -0300

    CM-222 Implementar auditoria en activity-be

--- src/main/java/com/cema/activity/services/client/administration/AdministrationClientService.java
+++ src/main/java/com/cema/activity/services/client/administration/AdministrationClientService.java
@@ -1,6 +1,10 @@
 package com.cema.activity.services.client.administration;
 
+import com.cema.activity.domain.audit.Audit;
+
 public interface AdministrationClientService {
 
     void validateEstablishment(String cuig);
+
+    void sendAuditRequest(Audit audit);
 }
