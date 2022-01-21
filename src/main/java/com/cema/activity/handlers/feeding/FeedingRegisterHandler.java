7dfa9e91cb1c9823048b21e6b5b7fc0af5137a20
Merlin Nuñez <merlinsn@gmail.com>
2022-01-20 22:16:11 -0300

    CM-254 Validar existencia de insumo en al cargar alimentacion

--- src/main/java/com/cema/activity/handlers/feeding/FeedingRegisterHandler.java
+++ src/main/java/com/cema/activity/handlers/feeding/FeedingRegisterHandler.java
@@ -9,6 +9,7 @@ import com.cema.activity.mapping.impl.FeedingMapper;
 import com.cema.activity.repositories.FeedingRepository;
 import com.cema.activity.services.authorization.AuthorizationService;
 import com.cema.activity.services.client.bovine.BovineClientService;
+import com.cema.activity.services.client.economic.EconomicClientService;
 import io.micrometer.core.instrument.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,18 +22,22 @@ public class FeedingRegisterHandler implements ActivityHandler<Feeding, Feeding>
     private final AuthorizationService authorizationService;
     private final FeedingRepository feedingRepository;
     private final BovineClientService bovineClientService;
+    private final EconomicClientService economicClientService;
 
     public FeedingRegisterHandler(FeedingMapper feedingMapper, AuthorizationService authorizationService,
-                                  FeedingRepository feedingRepository, BovineClientService bovineClientService) {
+                                  FeedingRepository feedingRepository, BovineClientService bovineClientService,
+                                  EconomicClientService economicClientService) {
         this.feedingMapper = feedingMapper;
         this.authorizationService = authorizationService;
         this.feedingRepository = feedingRepository;
         this.bovineClientService = bovineClientService;
+        this.economicClientService = economicClientService;
     }
 
     @Override
     public Feeding handle(Feeding activity) {
         String cuig = activity.getEstablishmentCuig();
+
 
         log.info("Handling registration of new feeding activity {}.", activity.getId());
 
@@ -44,6 +49,9 @@ public class FeedingRegisterHandler implements ActivityHandler<Feeding, Feeding>
         if (!StringUtils.isEmpty(tag)) {
             bovineClientService.validateBovine(tag, cuig);
         }
+
+        String food = activity.getFood();
+        economicClientService.validateSupply(food);
 
         CemaFeeding cemaFeeding = feedingMapper.mapDomainToEntity(activity);
 
---
+++ src/main/java/com/cema/activity/services/client/economic/impl/EconomicClientServiceImpl.java
@@ -0,0 +1,51 @@
+package com.cema.activity.services.client.economic.impl;
+
+import com.cema.activity.domain.ErrorResponse;
+import com.cema.activity.exceptions.ValidationException;
+import com.cema.activity.services.authorization.AuthorizationService;
+import com.cema.activity.services.client.economic.EconomicClientService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class EconomicClientServiceImpl implements EconomicClientService {
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String PATH_VALIDATE_SUPPLY = "supply/validate/{name}";
+
+    private final RestTemplate restTemplate;
+    private final String url;
+    private final AuthorizationService authorizationService;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public EconomicClientServiceImpl(RestTemplate restTemplate, @Value("${back-end.economic.url}") String url, AuthorizationService authorizationService) {
+        this.restTemplate = restTemplate;
+        this.url = url;
+        this.authorizationService = authorizationService;
+    }
+
+    @SneakyThrows
+    @Override
+    public void validateSupply(String food) {
+        String authToken = authorizationService.getUserAuthToken();
+        String searchUrl = url + PATH_VALIDATE_SUPPLY;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(AUTHORIZATION_HEADER, authToken);
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity entity = new HttpEntity("{}", httpHeaders);
+        try {
+            restTemplate.exchange(searchUrl, HttpMethod.GET, entity, Object.class, food);
+        } catch (RestClientResponseException httpClientErrorException) {
+            String response = httpClientErrorException.getResponseBodyAsString();
+            ErrorResponse errorResponse = mapper.readValue(response, ErrorResponse.class);
+            throw new ValidationException(errorResponse.getMessage(), httpClientErrorException);
+        }
+    }
+}
